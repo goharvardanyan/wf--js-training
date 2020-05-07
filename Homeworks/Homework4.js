f52cd234901099fe1d1a192a2c9a1394444d43ff
goharvardanyan <gohar.vardanyanc@gmail.com>
2020-05-07 21:23:09 +0400

    Homework4

--- Homeworks/Homework4.js
+++ Homeworks/Homework4.js
@@ -12,7 +12,7 @@ function shiftArrayEl(array, index = 0) {
     return shiftArrayEl(array, ++index)
 }
 
-
+console.log(shiftArrayEl([5]))
 
 //   Problem 2. Given an object. Invert it (keys become values and values become keys). 
 //    If there is more than key for that given value create an array.
@@ -74,56 +74,41 @@ function shiftElements(array, n) {
 console.log(shiftElements(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], 3))
 
 
-// Problem 6.
+// Problem 5.
 
 
-function subArray(array, length) {
-    let expectedLength = factorial(array.length) / (factorial(length) * factorial(array.length - length));
-    let newArr = [];
-    let count = 1;
-    let ind = 1;
-    
-    let tempArray = new Array(length);
-    for (let k = 0; k < length; k++) {
-        tempArray[k] = k;
-    }
-    
-    let tempArrayCopy = tempArray.slice(0);
-    while (newArr.length !== expectedLength) {
-        let subArray = [];
-        for (let j = 0; j < tempArray.length; j++) {   
-            subArray.push(array[tempArray[j]]);
+function createTree(array, resultObj = {},index = 0) {
+    let result = [];
+    for (let j = 1; j < array.length; j++) {
+        if (Object.values(array[index])[1] === Object.values(array[j])[0]){
+            result.push(Object.values(array[j]));
         }
-        newArr.push(subArray);
-        tempArray[length - 1] = tempArray[length - 1] + 1;
-        if (tempArray[length - 1] === array.length) {  
-            count++;
-                if (tempArray[length - count + 1] - tempArray[length - count] !== 1) {
-                    let arr = tempArray.slice(0, length - count);
-                    tempArray = tempArrayCopy.slice(length - count).map(val => val + ind);
-                    tempArray = arr.concat(tempArray);
-                    ind++;
-                    if (count === length) {
-                        count--;
-                    }
-                }
-                if (tempArray[length - 1] !== array.length - 1){
-                    count--;
-                } else {
-                    ind = tempArray[0] + 1;
-                }   
-        }      
+    }
+    let newObj = {};
+    if(result.length !== 0){
+        for (let i = 0; i < result.length; i++) {
+            newObj[result[i][1]] = {}; 
+            createTree(array, newObj,index = result[i][1]);
+            resultObj[result[0][0]] = newObj;
+        } 
+        return resultObj;      
     } 
-    return newArr;
+    if (result.length === 0){
+         newObj[index] = {};
+         return  newObj;
+    }    
 }
 
+console.log(createTree([
+    {p : null, id: 0},
+    {p : 0, id: 1},
+    {p : 0, id: 2},
+    {p : 1, id: 3},
+    {p : 1, id: 4},
+    {p : 2, id: 5},
+    {p : 4, id: 6},
+]))
 
-function factorial(n) {
-    let res = 1;
-    for(let i = 1; i <= n; i++) {
-        res = res * i;
-    } return res;
-}
 
 
 
