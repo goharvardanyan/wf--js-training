0f950d42210f6c881c474337d253b7c5ce965bca
goharvardanyan <gohar.vardanyanc@gmail.com>
2020-07-30 15:00:19 +0400

    Updates

---
+++ precache-manifest.725f82a99d704b2e6dc3a5b072d00d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982cd99bb4de1134fe6f3bf25e29de4d",
+    "url": "/ToDo_React/index.html"
+  },
+  {
+    "revision": "e7858c82e5d63fb8c152",
+    "url": "/ToDo_React/static/css/main.5c28a377.chunk.css"
+  },
+  {
+    "revision": "27a52e54262f92afbeec",
+    "url": "/ToDo_React/static/js/2.3b208e95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ToDo_React/static/js/2.3b208e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7858c82e5d63fb8c152",
+    "url": "/ToDo_React/static/js/main.6fa305d0.chunk.js"
+  },
+  {
+    "revision": "b5cd6641ed148dda20e1",
+    "url": "/ToDo_React/static/js/runtime-main.bf644212.js"
+  }
+]);
